49146807bbfefcded543f0e22d22acdb9cdc5157
navyal31051999 <navyall1999@gmail.com>
2023-10-02 11:44:35 +0530

    Filtering

--- WebapiDemo/Controllers/ProductController.cs
+++ WebapiDemo/Controllers/ProductController.cs
@@ -35,9 +35,10 @@ namespace WebapiDemo.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
+            string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductWithTypesAndBrandsSpecification(sort,brandId,typeId);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>
             (products));
