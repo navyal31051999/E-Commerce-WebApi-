e4860304e97cb0f9cd4e757a01cb7eda5f737c89
navyal31051999 <navyall1999@gmail.com>
2023-10-03 16:18:24 +0530

    search funtionality with name

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -11,14 +11,22 @@ namespace Core.Specifications
         public int PageIndex { get; set; } = 1;
         private int _pageSize = 6;
 
-        public int PageSize 
+        public int PageSize
         {
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
 
-        public int? BrandId{get; set; }
-        public int? TypeId{get; set; }
-        public string Sort{get; set; }
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string Sort { get; set; }
+
+        private string _search;
+
+        public string Search
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
     }
 }
--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -9,14 +9,17 @@ namespace Core.Specifications
 {
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecification(ProductSpecParams productParams) : base(x =>
+        public ProductWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+        : base(x =>
+       (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+       (productParams.Search)) &&
         (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
         (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
             AddOrderBy(x => x.Name);
-            ApplyPaging(productParams.PageSize * (productParams.PageIndex -1 ),
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1),
             productParams.PageSize);
 
             if (!string.IsNullOrEmpty(productParams.Sort))
